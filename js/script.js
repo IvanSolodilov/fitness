d98324deac4e9e90323d3e47cac742bd8ad8d86e
IvanSolodilov <82935452+IvanSolodilov@users.noreply.github.com>
2023-12-19 13:20:44 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,12 +7,11 @@ class User {
   }
 }
 
-let username;
-let user1;
-function getValue() {
+let user = new User();
 
+function getValue() {
   username = document.getElementById("username").value;
   console.log("Значение input: " + username);
-  user1 = new User(username);
-  console.log(user1)
+  user.addUsername(username);
+  console.log(user);
 }
